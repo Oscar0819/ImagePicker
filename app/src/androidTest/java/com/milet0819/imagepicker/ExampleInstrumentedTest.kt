45be46f0b37bf51f394da1de84edab0ee59c63ec
Park Eunwoo <dn08193@gmail.com>
2025-02-05 18:10:51 +0900

    [테스트 코드]

--- app/src/androidTest/java/com/milet0819/imagepicker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/milet0819/imagepicker/ExampleInstrumentedTest.kt
@@ -1,12 +1,22 @@
 package com.milet0819.imagepicker
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +25,26 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityScenarioRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.milet0819.imagepicker", appContext.packageName)
+    }
+
+    @Test
+    fun checkButton() {
+        onView(withId(R.id.btImagePicker))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun checkTextView() {
+        onView(withId(R.id.textView)).check(matches(withText("Hello World!")))
     }
 }
